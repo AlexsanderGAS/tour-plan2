1055feb7ec60d2be3c10fa41ce826c8585882c93
AlexsanderGAS <alexsandrhuryn@gmail.com>
2021-06-25 22:12:16 +0300

    create: footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// $(document).ready(function(){
+//   $('.lider-reviews__inner').slick({
+//     slidesToShow: 1,
+//   });
+// });
+$('.newsletter').parallax({imageSrc: '/images/newsletter-img.jpg'});
+
+
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     loop: true,
@@ -9,3 +17,4 @@ const swiper = new Swiper('.swiper-container', {
     },
   
   });
+  
